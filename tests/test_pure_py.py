076fa87e104c5d5d9fb182e06ffa92a89d14d9f6
sebdroid <me@sebjo.se>
2023-04-07 14:15:12 +0100

    Tests

--- tests/test_pure_py.py
+++ tests/test_pure_py.py
@@ -1,5 +1,12 @@
 import pure_checker
 
+def test_zip_bomb():
+    with open("tests/zbsm.zip", "rb") as f:
+        purity = pure_checker.pure_zip(f.read())
+        assert purity == 7
+        assert pure_checker.pure_error_code(purity) == "PURE_E_ZIP_BOMB_FIFIELD"
+        assert pure_checker.pure_error_string(purity) == "zip bomb: local file header overlap (see research by David Fifield)"
+
 def test_pure_error_code():
     assert pure_checker.pure_error_code(5) == "PURE_E_ZIP_BOMB_ARCHIVES"
 
